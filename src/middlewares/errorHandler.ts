9a53962ba669de3f6a95c215c7fcbd3df6ffef39
ekadk <ekadharmakusuma3@gmail.com>
2023-04-15 01:13:59 +0700

    ADD: auth unique constraint violation handler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,12 +1,22 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { ErrorRequestHandler } from "express";
 
 export const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
-  let code = 500;
-  let message = "Internal server error";
+  console.log(error);
+  let code: number = 500;
+  let message: string = "Internal server error";
 
   if (error.name === "VALIDATION_ERROR") {
     code = error.code;
     message = error.message;
+  }
+
+  if (error instanceof PrismaClientKnownRequestError) {
+    if (error.code === "P2002") {
+      code = 400;
+      const target: string | undefined | unknown = error.meta?.target;
+      message = `value in ${target} is violating unique constraint`;
+    }
   }
 
   res.status(code).json({ message });
