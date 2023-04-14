5d8cae96d577b65e0361127e0b1e3b4a1f3b1d1d
ekadk <ekadharmakusuma3@gmail.com>
2023-04-15 02:04:20 +0700

    REFACTOR: error object in validate middleware

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,15 +1,15 @@
 import { Request, RequestHandler } from "express";
 import { ValidationChain, validationResult } from "express-validator";
 
+export const VALIDATION_ERROR = {
+  name: "VALIDATION_ERROR",
+  code: 400,
+};
 const validationChecker: RequestHandler = async (req, res, next) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      throw {
-        name: "VALIDATION_ERROR",
-        code: 400,
-        message: errors.array()[0].msg,
-      };
+      throw { ...VALIDATION_ERROR, message: errors.array()[0].msg };
     }
     next();
   } catch (error) {
