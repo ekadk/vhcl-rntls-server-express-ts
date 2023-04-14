58ade3294c596a6fba63b65a7b33691ecfdc8bf7
ekadk <ekadharmakusuma3@gmail.com>
2023-04-15 02:06:08 +0700

    FEAT: hash password & create token on signup/login; REFACTOR: invalid signin error

--- src/features/auth/handler.ts
+++ src/features/auth/handler.ts
@@ -1,19 +1,19 @@
 import { RequestHandler } from "express";
 import prisma from "../../db";
+import { createToken } from "./helpers";
+import * as argon from "argon2";
 
 const localSignUpHandler: RequestHandler = async (req, res, next) => {
   try {
+    const hash = await argon.hash(req.body.password);
     const user = await prisma.user.create({
       data: {
         email: req.body.email,
-        password: req.body.password,
+        password: hash,
       },
     });
-    res.status(201).json({
-      id: user.id,
-      email: user.email,
-      createdAt: user.createdAt,
-    });
+    const access_token = createToken(user);
+    res.status(201).json({ access_token });
   } catch (error) {
     next(error);
   }
@@ -24,16 +24,24 @@ const localSigninHandler: RequestHandler = async (req, res, next) => {
     const user = await prisma.user.findUnique({
       where: { email: req.body.email },
     });
-    if (!user)
-      throw { name: "INVALID_SIGNIN", message: "invalid email or password!" };
-
-    if (req.body.password !== user.password)
-      throw { name: "INVALID_SIGNIN", message: "invalid email or password!" };
-
-    res.status(200).json({ message: "signed in!" });
+    if (!user) throw INVALID_SIGININ;
+    const passwordIsValid = await argon.verify(
+      user.password,
+      req.body.password
+    );
+    if (!passwordIsValid) throw INVALID_SIGININ;
+    const access_token = createToken(user);
+    res.status(200).json({ access_token });
   } catch (error) {
+    console.log(error);
     next(error);
   }
 };
 
-export { localSignUpHandler, localSigninHandler };
+const INVALID_SIGININ = {
+  code: 403,
+  name: "INVALID_SIGNIN",
+  message: "invalid email or password!",
+};
+
+export { localSignUpHandler, localSigninHandler, INVALID_SIGININ };
