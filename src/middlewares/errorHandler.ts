c47c3634d7195b41326055a388b52f109b7d81a3
ekadk <ekadharmakusuma3@gmail.com>
2023-04-15 00:50:06 +0700

    ADD: validation error response

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -4,5 +4,10 @@ export const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   let code = 500;
   let message = "Internal server error";
 
+  if (error.name === "VALIDATION_ERROR") {
+    code = error.code;
+    message = error.message;
+  }
+
   res.status(code).json({ message });
 };
