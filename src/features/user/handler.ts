d0a5bdfd8db7467459a0051c576a1e9c1a831cf5
ekadk <ekadharmakusuma3@gmail.com>
2023-04-15 17:08:13 +0700

    FEAT: update user profile

--- src/features/user/handler.ts
+++ src/features/user/handler.ts
@@ -26,12 +26,37 @@ export const createUserProfileHandler: RequestHandler = async (
 export const getUserProfileHandler: RequestHandler = async (req, res, next) => {
   try {
     const userPayload = JSON.parse(req.headers.user as string);
-    console.log({ userPayload });
     const profile = await prisma.userProfile.findUnique({
       where: { userId: userPayload.id },
     });
     if (!profile) throw USER_PROFILE_NOT_FOUND;
     res.status(200).json({ profile });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUserProfileHandler: RequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  try {
+    const userPayload = JSON.parse(req.headers.user as string);
+    const profile = await prisma.userProfile.findUnique({
+      where: { userId: userPayload.id },
+    });
+    if (!profile) throw USER_PROFILE_NOT_FOUND;
+    const updatedProfile = await prisma.userProfile.update({
+      where: { userId: userPayload.id },
+      data: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phoneNumber: req.body.phoneNumber,
+        address: req.body.address,
+      },
+    });
+    res.status(200).json({ profile: { ...updatedProfile } });
   } catch (error) {
     next(error);
   }
--- src/features/user/router.ts
+++ src/features/user/router.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { createUserProfileHandler, getUserProfileHandler } from "./handler";
+import {
+  createUserProfileHandler,
+  getUserProfileHandler,
+  updateUserProfileHandler,
+} from "./handler";
 import createProfileValidator from "./validator";
 
 const router = Router();
 
 router.post("/profile", createProfileValidator(), createUserProfileHandler);
 router.get("/profile", getUserProfileHandler);
+router.patch("/profile", updateUserProfileHandler);
 
 export { router as userRouter };
