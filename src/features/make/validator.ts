b8ed65d47282af6d3c3bafc10cc4fe3c1a28b741
ekadk <ekadharmakusuma3@gmail.com>
2023-04-19 22:13:52 +0700

    FIX: make validator error message

--- src/features/make/validator.ts
+++ src/features/make/validator.ts
@@ -1,13 +1,14 @@
 import { body } from "express-validator";
 import validate from "../../middlewares/validate";
 
-export default function createCategoryValidator() {
+export default function createMakeValidator() {
   return validate([
     body("name")
       .notEmpty()
-      .withMessage("category name is required!")
+      .withMessage("make name is required!")
       .isString()
       .isLength({ max: 255 })
-      .withMessage("maximum length of category name is 255 characters!"),
+      .withMessage("maximum length of make name is 255 characters!"),
+    body("imgUrl").notEmpty().withMessage("make imageUrl is required!"),
   ]);
 }
