8b2cc1cfe8ce5574c6f438aab70d31041ee2cd77
ekadk <ekadharmakusuma3@gmail.com>
2023-04-20 04:44:16 +0700

    FIX: validator

--- src/features/model/validator.ts
+++ src/features/model/validator.ts
@@ -1,14 +1,18 @@
 import { body } from "express-validator";
 import validate from "../../middlewares/validate";
 
-export default function createMakeValidator() {
+export default function createModelValidator() {
   return validate([
     body("name")
       .notEmpty()
-      .withMessage("make name is required!")
+      .withMessage("name is required!")
       .isString()
       .isLength({ max: 255 })
       .withMessage("maximum length of make name is 255 characters!"),
-    body("imgUrl").notEmpty().withMessage("make imageUrl is required!"),
+
+    body("pricePerDay").notEmpty().withMessage("pricePerDay is required!").isNumeric(),
+    body("imgUrl").notEmpty().withMessage("imgUrl is required!"),
+    body("makeId").notEmpty().withMessage("makeId is required!").isNumeric(),
+    body("categoryId").notEmpty().withMessage("categoryId is required!").isNumeric(),
   ]);
 }
